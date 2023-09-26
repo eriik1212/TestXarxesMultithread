422cd7d687d1802710ca0d101fe950fbb2906ca9
Gerard <gerhoquei8@gmail.com>
2023-09-26 11:53:36 +0200

    simple movement

---
+++ SandBoxGEDA/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float forceAmount = 5.0f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(moveHorizontal, 0.0f, moveVertical) * forceAmount;
+        rb.AddForce(force);
+    }
+}
+
+
